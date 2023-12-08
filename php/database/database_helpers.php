b91a9ff328a5d293ba01d00cf5f8a9faba08640d
bombies <xjamesdzn@gmail.com>
2023-12-08 10:03:48 -0500

    Setup database connection

--- php/database/database_helpers.php
+++ php/database/database_helpers.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'localhost:3306'; // Had to specify port due to conflicts with existing databases on my host machine
-$username = 'lab5_user';
-$password = 'password123';
-$dbname = 'world';
-$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+require_once '../../env.php';
 
-?>
+$host = $_ENV['DATABASE_HOST']; // Had to specify port due to conflicts with existing databases on my host machine
+$username = $_ENV['DATABASE_USERNAME'];
+$password = $_ENV['DATABASE_PASSWORD'];
+$dbname = $_ENV['DATABASE_NAME'];
+
+$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
