b77ccef3ec1dd93bdc15fd5b21bcb7ff124fa08d
nathansmith <nathan2012smith@gmail.com>
2023-12-11 00:22:54 -0500

    Implement get all notes

---
+++ api/notes/getnotes.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../utils/api_utils.php';
+require_once '../database/database_helpers.php';
+
+handle_get(function (){
+    $params = $_GET;
+    $contactid = isset($params['contactid']) ? $params['contactid'] : null;
+    $sql = "SELECT notes.content, notes.contact_id, users.firstname, users.lastname FROM notes\n"
+
+    . "JOIN users\n"
+
+    . "ON notes.created_by = users.id;";
+    $notes = query($sql, [$contactid])->fetchAll(PDO::FETCH_ASSOC);
+    return json_encode($notes);
+})
+
+?>
