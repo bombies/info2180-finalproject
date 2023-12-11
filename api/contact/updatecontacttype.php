0693b4e2527060679bf89696ecdefb0fbdc7cd6a
bombies <xjamesdzn@gmail.com>
2023-12-11 08:50:04 -0500

    Fix some php

--- api/contact/updatecontacttype.php
+++ api/contact/updatecontacttype.php
@@ -3,7 +3,7 @@
 require_once '../utils/api_utils.php';
 require_once '../database/database_helpers.php';
 
-handle_post(function (){
+handle_post(function () {
     $body = $_POST;
     $contactid = isset($body['contactid']) ? $body['contactid'] : null;
     $type = isset($body['type']) ? $body['type'] : null;
@@ -11,17 +11,14 @@ handle_post(function (){
 
     if (!$response) {
         http_response_code(500);
-        return json_encode(['error' => 'Failed to create note']);
+        return ['error' => 'Failed to create note'];
     }
 
     http_response_code(200);
-    $sql = $sql = "SELECT CONCAT(contacts.title, '. ', contacts.firstname, ' ', contacts.lastname) AS fullname, \n"
-
-    . "contacts.email, contacts.company, contacts.telephone, contacts.created_at, contacts.updated_at, contacts.type,\n"
-
-    . "CONCAT(users.firstname, ' ',users.lastname) AS userfullname FROM contacts\n"
-
-    . "JOIN users WHERE contacts.id = ?;\n";
+    $sql = "SELECT CONCAT(contacts.title, '. ', contacts.firstname, ' ', contacts.lastname) AS fullname, \n"
+        . "contacts.email, contacts.company, contacts.telephone, contacts.created_at, contacts.updated_at, contacts.type,\n"
+        . "CONCAT(users.firstname, ' ',users.lastname) AS userfullname FROM contacts\n"
+        . "JOIN users WHERE contacts.id = ?;\n";
     $updatedContact = query($sql, [$contactid])->fetch(PDO::FETCH_ASSOC);
     return $updatedContact;
 })
