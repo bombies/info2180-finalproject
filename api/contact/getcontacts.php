bfa42564ada2b25de34f3842a92a37a0fe154589
nathansmith <nathan2012smith@gmail.com>
2023-12-11 00:59:20 -0500

    edited getcontacts

--- api/contact/getcontacts.php
+++ api/contact/getcontacts.php
@@ -4,7 +4,7 @@ require_once '../utils/api_utils.php';
 require_once '../database/database_helpers.php';
 
 handle_get(function (){
-    $contacts = query("SELECT CONCAT(title, '. ', firstname, ' ', lastname) AS fullname, email, company, type from contacts;")->fetchAll(PDO::FETCH_ASSOC);
+    $contacts = query("SELECT CONCAT(title, '. ', firstname, ' ', lastname) AS fullname, email, company, type, id from contacts;")->fetchAll(PDO::FETCH_ASSOC);
     return json_encode($contacts);
 })
 ?>
